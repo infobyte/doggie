0b34eaf98860ab73158d7bf636752b54c781292c
Gaston Aznarez <gaznarez@faradaysec.com>
2024-10-25 16:34:19 -0300

    Change from _io_sync to only _io for serial

--- slcan/src/lib.rs
+++ slcan/src/lib.rs
@@ -49,12 +49,6 @@ impl CanFrame {
             return None;
         }
 
-        if len % 2 != 0 {
-            return None;
-        }
-
-        let len = len / 2;
-
         let mut frame = CanFrame {
             id: id.into(),
             is_remote,
