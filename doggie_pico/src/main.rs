f9b5824f832ad67b71b1213a1c2c181a21890eef
Octavio Gianatiempo <ogianatiempo@faradaysec.com>
2024-11-21 12:35:13 -0300

    DoggiePico serial over usb

--- doggie_pico/src/main.rs
+++ doggie_pico/src/main.rs
@@ -9,16 +9,22 @@
 
 mod soft_timer;
 mod spi_device;
+mod usb_device;
 
 use soft_timer::SoftTimer;
 use spi_device::CustomSpiDevice;
-use defmt::*;
+use defmt::{error, info};
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::peripherals::SPI0;
 use embassy_rp::peripherals::UART0;
+use embassy_rp::peripherals::USB;
 use embassy_rp::spi::{Blocking, Spi};
 use embassy_rp::uart::{BufferedInterruptHandler, BufferedUart, Config};
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::UsbDevice;
+use usb_device::UsbWrapper;
 use gpio::{Level, Output};
 use doggie_core::*;
 use embassy_rp::{gpio, spi};
@@ -26,28 +32,107 @@ use mcp2515::MCP2515;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
+#[cfg(feature = "uart")]
 bind_interrupts!(struct Irqs {
     UART0_IRQ => BufferedInterruptHandler<UART0>;
 });
 
+#[cfg(feature = "usb")]
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[embassy_executor::task]
+async fn usb_task(mut usb: UsbDevice<'static, Driver<'static, USB>>) -> ! {
+    usb.run().await
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    if !cfg!(feature = "uart") && !cfg!(feature = "usb") {
+        panic!("Either 'uart' or 'usb' feature must be enabled.");
+    }
+
     let p = embassy_rp::init(Default::default());
 
-    // Setup UART
-    let (tx_pin, rx_pin, uart_no) = (p.PIN_0, p.PIN_1, p.UART0);
+    #[cfg(feature = "uart")]
+    let serial = {
+        // Setup UART
+        let (tx_pin, rx_pin, uart_no) = (p.PIN_0, p.PIN_1, p.UART0);
 
-    let mut uart_config = Config::default();
-    uart_config.baudrate = 115200;
+        let mut uart_config = Config::default();
+        uart_config.baudrate = 115200;
 
-    static TX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
-    let tx_buf = &mut TX_BUF.init([0; 16])[..];
+        static TX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
+        let tx_buf = &mut TX_BUF.init([0; 16])[..];
 
-    static RX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
-    let rx_buf = &mut RX_BUF.init([0; 16])[..];
-    let uart = BufferedUart::new(uart_no, Irqs, tx_pin, rx_pin, tx_buf, rx_buf, uart_config);
+        static RX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
+        let rx_buf = &mut RX_BUF.init([0; 16])[..];
+        let serial = BufferedUart::new(uart_no, Irqs, tx_pin, rx_pin, tx_buf, rx_buf, uart_config);
 
-    info!("UART init ok");
+        info!("UART init ok");
+
+        serial
+    };
+
+    #[cfg(feature = "usb")]
+    let serial = {
+        // Create the driver, from the HAL.
+        let driver = Driver::new(p.USB, Irqs);
+
+        // Create embassy-usb Config
+        let config = {
+            let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+            config.manufacturer = Some("Aznarez/Gianatiempo");
+            config.product = Some("DoggiePico");
+            config.serial_number = Some("1337");
+            config.max_power = 100;
+            config.max_packet_size_0 = 64;
+
+            // Required for windows compatibility.
+            // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
+            config.device_class = 0xEF;
+            config.device_sub_class = 0x02;
+            config.device_protocol = 0x01;
+            config.composite_with_iads = true;
+            config
+        };
+
+        // Create embassy-usb DeviceBuilder using the driver and config.
+        // It needs some buffers for building the descriptors.
+        let mut builder = {
+            static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+            static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+            static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+
+            let builder = embassy_usb::Builder::new(
+                driver,
+                config,
+                CONFIG_DESCRIPTOR.init([0; 256]),
+                BOS_DESCRIPTOR.init([0; 256]),
+                &mut [], // no msos descriptors
+                CONTROL_BUF.init([0; 64]),
+            );
+            builder
+        };
+
+        // Create classes on the builder.
+        let mut class = {
+            static STATE: StaticCell<State> = StaticCell::new();
+            let state = STATE.init(State::new());
+            CdcAcmClass::new(&mut builder, state, 64)
+        };
+
+        // Build the builder.
+        let usb = builder.build();
+
+        // Run the USB device.
+        spawner.spawn(usb_task(usb)).unwrap();
+
+        class.wait_connection().await;
+
+        UsbWrapper::new(class)
+    };
 
     // Setup SPI
     let (clk_pin, tx_pin, rx_pin, cs_pin, spi_no) =
@@ -65,7 +150,7 @@ async fn main(spawner: Spawner) {
 
     // Create the Bsp
     // let bsp = Bsp::new(can, uart);
-    let bsp = Bsp::new_with_mcp2515(spi, delay, uart);
+    let bsp = Bsp::new_with_mcp2515(spi, delay, serial);
 
     info!("MCP2515 init ok");    
 
@@ -75,7 +160,13 @@ async fn main(spawner: Spawner) {
     core_run!(core);
 }
 
+#[cfg(feature = "uart")]
+type SerialType = BufferedUart<'static, UART0>;
+
+#[cfg(feature = "usb")]
+type SerialType = UsbWrapper<'static>;
+
 core_create_tasks!(
-    BufferedUart<'static, UART0>,
+    SerialType,
     MCP2515<CustomSpiDevice<'static, SPI0, Blocking>>
 );
---
+++ doggie_pico/src/usb_device.rs
@@ -0,0 +1,50 @@
+use defmt::{error, println};
+use embassy_rp::{peripherals::USB, usb::Driver};
+use embassy_usb::class::cdc_acm::CdcAcmClass;
+use embedded_io::{ErrorKind, ErrorType};
+use embedded_io_async::{Error, Read, Write};
+
+pub struct UsbWrapper<'d> {
+    usb: CdcAcmClass<'d, Driver<'d, USB>>,
+}
+
+impl<'d> UsbWrapper<'d> {
+    pub fn new(usb: CdcAcmClass<'d, Driver<'d, USB>>) -> Self {
+        UsbWrapper { usb }
+    }
+}
+
+#[derive(Debug)]
+pub struct UsbError {}
+
+impl<'d> Error for UsbError {
+    fn kind(&self) -> ErrorKind {
+        ErrorKind::Other
+    }
+}
+
+impl<'d> ErrorType for UsbWrapper<'d> {
+    type Error = UsbError;
+}
+
+impl<'d> Read for UsbWrapper<'d> {
+    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        match self.usb.read_packet(buf).await {
+            Err(_) => Err(UsbError {}),
+            Ok(size) => Ok(size),
+        }
+    }
+}
+
+impl<'d> Write for UsbWrapper<'d> {
+    async fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        println!("WRITING: {}", buf);
+        match self.usb.write_packet(buf).await {
+            Err(_) => {
+                error!("Error on the usb write");
+                Err(UsbError {})
+            }
+            Ok(_) => Ok(buf.len()),
+        }
+    }
+}
