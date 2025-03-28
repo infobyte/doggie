7e1960be0c34b1649cebc18e2a424ae480b9f126
Gaston Aznarez <gaznarez@faradaysec.com>
2025-03-28 15:15:34 -0300

    [core] Bigger CAN channel

--- doggie_core/src/types.rs
+++ doggie_core/src/types.rs
@@ -2,7 +2,7 @@ use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::{Channel, Receiver, Sender};
 use slcan::SlcanCommand;
 
-const CAN_CHANNEL_SIZE: usize = 16;
+const CAN_CHANNEL_SIZE: usize = 32;
 
 pub type CanChannel = Channel<CriticalSectionRawMutex, SlcanCommand, CAN_CHANNEL_SIZE>;
 pub type CanChannelSender =
