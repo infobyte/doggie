478b497ca925c4a0c6a0741ad688f90b177e528a
Gaston Aznarez <gaznarez@faradaysec.com>
2025-04-24 11:34:03 -0300

    Evil core optimizations

--- evil_core/src/attack_machine.rs
+++ evil_core/src/attack_machine.rs
@@ -1,15 +1,9 @@
-use crate::commands::{AttackCmd, FastBitStack};
 use crate::attack_errors::AttackError;
+use crate::commands::{AttackCmd, FastBitStack};
 use crate::tranceiver::Tranceiver;
+use crate::TranceiverState;
 
 const MAX_ATTACK_SIZE: usize = 32;
-
-
-enum State {
-    START,
-    MIDDLE,
-    END,
-}
 
 pub struct AttackMachine<Tr>
 where
@@ -21,10 +15,11 @@ where
     buffer: FastBitStack,
     bit_stuffing_cnt: u8,
     bit_stuffing_polarity: bool,
-    state: State,
+    on_start: bool,
+    next_state: TranceiverState,
 }
 
-impl <Tr> AttackMachine <Tr>
+impl<Tr> AttackMachine<Tr>
 where
     Tr: Tranceiver,
 {
@@ -38,14 +33,14 @@ where
             buffer: FastBitStack::new(),
             bit_stuffing_cnt: 0,
             bit_stuffing_polarity: true,
-            state: State::START,
+            on_start: true,
+            next_state: TranceiverState::new(),
         }
     }
 
-
     pub fn arm(&mut self, attack: &[AttackCmd]) -> Result<(), AttackError> {
         self.index = 0;
-        self.state = State::START;
+        self.on_start = true;
         self.bit_stuffing_polarity = true;
         self.bit_stuffing_cnt = 0;
         self.buffer.clean();
@@ -61,162 +56,138 @@ where
                 self.attack[index] = AttackCmd::None;
             }
         }
-        
+
+        self.pre_calculate();
+
         Ok(())
     }
 
-    #[inline]
-    fn next_cmd(&mut self) -> bool {
-        self.index += 1;
-
-        return self.index < self.attack.len()
-    }
-
-    #[inline]
-    pub fn handle_start(&mut self) -> bool {
+    #[inline(always)]
+    fn pre_calculate(&mut self) {
+        /* Pre calculate the next state of the tranceiver */
         match self.attack[self.index] {
             AttackCmd::Wait { ref mut bits } => {
                 // Wait for one bit more
                 if *bits > 0 {
                     *bits -= 1;
                 }
-
-                true
-            },
+            }
             AttackCmd::Force { ref mut stream } => {
-                self.tranceiver.set_force(stream.pop());
-                true
-            },
+                self.next_state.set_force(stream.pop());
+            }
             AttackCmd::Send { ref mut stream } => {
-                self.tranceiver.set_tx(!stream.pop());
-                true
-            },
+                self.next_state.set_tx(!stream.pop());
+            }
             AttackCmd::WaitBuffered => {
                 let value = self.buffer.value() * 8;
                 self.buffer.clean();
 
                 if value == 0 {
                     self.index += 1;
-                    self.handle_start()
+                    // We shouldn't have two WaitBuffered together
+                    self.pre_calculate();
                 } else {
-                    self.attack[self.index] = AttackCmd::Wait { bits: (value - 1) as usize };
-                    true
+                    self.attack[self.index] = AttackCmd::Wait {
+                        bits: (value - 1) as usize,
+                    };
                 }
             }
-            _ => true
+            _ => {}
         }
     }
 
-    #[inline]
-    pub fn handle_middle(&mut self) -> bool {
+    #[inline(always)]
+    pub fn handle_middle(&mut self, rx: bool) -> Result<bool, ()> {
+        /* Handle the middle of the bit with the rx state
+         * Returns
+         *     Err(()) => if we need to stop the execution
+         *     Ok(true) => if we need to pass to the next cmd
+         *     Ok(false) => If we are in the same command
+         * */
         match self.attack[self.index] {
+            AttackCmd::Wait { bits } => Ok(bits <= 0),
+            AttackCmd::Force { ref mut stream } => {
+                if stream.len() <= 0 {
+                    self.next_state.set_force(false);
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            AttackCmd::Send { ref mut stream } => {
+                if stream.len() <= 0 {
+                    self.next_state.set_tx(true);
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
             AttackCmd::Match { ref mut stream } => {
                 // Check the next bit with the RX
                 // If it doesn't match, finish the attack
-                stream.pop() == self.tranceiver.get_rx()
-            },
-            AttackCmd::Read { ref mut len } => {
-                self.buffer.push(self.tranceiver.get_rx());
-                *len -= 1;
-                true
-            },
-            _ => true
-        }
-    }
+                let result = rx == stream.pop();
 
-    #[inline]
-    pub fn handle_end(&mut self) -> bool {
-        match self.attack[self.index] {
-            AttackCmd::None => {
-                false
-            },
-            AttackCmd::Wait { bits } => {
-                if bits <= 0 {
-                    self.next_cmd()
+                if !result {
+                    Err(())
                 } else {
-                    true
+                    Ok(stream.len() <= 0)
                 }
-
             }
-            AttackCmd::Force { ref mut stream } => {
-                if stream.len() <= 0 {
-                    self.tranceiver.set_force(false);
-                    self.next_cmd()
-                } else {
-                    true
-                }
-            },
-            AttackCmd::Send { ref mut stream } => {
-                if stream.len() <= 0 {
-                    self.tranceiver.set_tx(true);
-                    self.next_cmd()
-                } else {
-                    true
-                }
-            },
-            AttackCmd::Match { ref mut stream } => {
-                // Check the next bit with the RX
-                if stream.len() <= 0 {
-                    self.next_cmd()
-                } else {
-                    true
-                }
-            },
             AttackCmd::Read { ref mut len } => {
-                if *len <= 0 {
-                    self.next_cmd()
-                } else {
-                    true
-                }
-            },
-            _ => true
+                self.buffer.push(rx);
+                *len -= 1;
+
+                Ok(*len <= 0)
+            }
+            _ => Ok(false),
         }
     }
 
-    #[inline]
+    #[inline(always)]
+    fn next_cmd(&mut self) -> bool {
+        self.index += 1;
+
+        return self.index < self.attack.len();
+    }
+
+    #[inline(always)]
     pub fn handle(&mut self) -> Option<u32> {
-        match self.state {
-            State::START => {
-                self.state = State::MIDDLE;
+        if self.on_start {
+            self.tranceiver.apply(&self.next_state);
+            self.on_start = false;
 
-                if self.handle_start() {
-                    Some(2)
-                } else {
-                    None
+            Some(1)
+        } else {
+            let rx = self.tranceiver.get_rx();
+
+            if rx == self.bit_stuffing_polarity {
+                self.bit_stuffing_cnt += 1;
+            } else {
+                self.bit_stuffing_cnt = 1;
+                self.bit_stuffing_polarity = rx;
+            }
+
+            // handle_middle return false if we finished the attack
+            match self.handle_middle(rx) {
+                Ok(true) => {
+                    if !self.next_cmd() {
+                        return None;
+                    }
                 }
-            },
-            State::MIDDLE => {
-                let rx = self.tranceiver.get_rx();
+                Err(_) => return None,
+                Ok(_) => {}
+            };
 
-                if rx == self.bit_stuffing_polarity {
-                    self.bit_stuffing_cnt += 1;
-                } else {
-                    self.bit_stuffing_cnt = 1;
-                    self.bit_stuffing_polarity = rx;
-                }
+            // Bit stuffing
+            if self.bit_stuffing_cnt >= 5 {
+                self.bit_stuffing_cnt = 0;
 
-                self.state = State::END;
-                
-                if self.handle_middle() {
-                    Some(6)
-                } else {
-                    None
-                }
-            },
-            State::END => {
-                if !self.handle_end() {
-                    return None
-                }
-
-                self.state = State::START;
-
-                if self.bit_stuffing_cnt >= 5 {
-                    self.bit_stuffing_cnt = 0;
-
-                    Some(AttackMachine::<Tr>::QUANTA_PER_BIT as u32)
-                } else {
-                    Some(0)
-                }
+                Some(8)
+            } else {
+                self.on_start = true;
+                // pre_calculate
+                self.pre_calculate();
+                Some(7)
             }
         }
     }
