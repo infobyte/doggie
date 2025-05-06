ea43d396d4d8f40a29fd8b1c99bf1e035b7811d5
Gaston Aznarez <gaznarez@faradaysec.com>
2025-05-06 11:46:48 -0300

    [esp32] AIO binary now uses BLE and wired serial (UART or USB)

--- doggie_esp32/src/doggie_esp32.rs
+++ doggie_esp32/src/doggie_esp32.rs
@@ -4,10 +4,12 @@
 mod ble;
 mod twai_can;
 mod logging;
+mod serial_mux;
 
 use twai_can::CanWrapper;
 use ble::{BleSerial, BleServer, PIPE_CAPACITY};
 use logging::init_logs;
+use serial_mux::SerialMux;
 
 use doggie_core::*;
 
@@ -28,10 +30,12 @@ use esp_hal::{
         Uart, UartTx,
     },
     peripherals,
+    Async,
 };
 use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use embedded_io_async::{Read, Write};
 use static_cell::StaticCell;
+
 
 
 static mut BLE_TX_PIPE: Pipe<CriticalSectionRawMutex, PIPE_CAPACITY> = Pipe::new();
@@ -55,16 +59,17 @@ pub async fn ble_task(mut server: BleServer<'static>) {
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // esp_println::logger::init_logger_from_env();
-
+    // Board initialization
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
         config
     });
 
+    // Heap initialization needed by the BLE
     esp_alloc::heap_allocator!(72 * 1024);
 
+    // Setup embassy timer
     cfg_if::cfg_if! {
         if #[cfg(feature = "esp32")] {
             let timg1 = TimerGroup::new(peripherals.TIMG1);
@@ -76,10 +81,11 @@ async fn main(spawner: Spawner) {
         }
     }
 
-    
+    // Blink initialization
     let led = Output::new(peripherals.GPIO8, Level::Low);
     spawner.spawn(blink_task(led)).unwrap();
     
+    // Serial logging initialization
     let mut dbg_serial = {
         let (tx_pin, rx_pin) = (peripherals.GPIO3, peripherals.GPIO2);
         let mut config = esp_hal::uart::Config::default().baudrate(115200);
@@ -91,6 +97,7 @@ async fn main(spawner: Spawner) {
     let (_, dbg_tx) = dbg_serial.split();
     init_logs(dbg_tx);
 
+    // BLE initialization
     let (ble_tx_reader, ble_tx_writer) = unsafe { BLE_TX_PIPE.split() };
     let (ble_rx_reader, ble_rx_writer) = unsafe { BLE_RX_PIPE.split() };
 
@@ -109,14 +116,35 @@ async fn main(spawner: Spawner) {
         ble_tx_writer, ble_rx_reader
     );
 
+
+    // Wired serial initialization
+    #[cfg(feature = "esp32c3")]
+    let wired_serial = UsbSerialJtag::new(peripherals.USB_DEVICE).into_async();
+
+    // Setup UART (using these pins, also passes through USB)
+    #[cfg(not(feature = "esp32c3"))]
+    let wired_serial = {
+        let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
+        let config = esp_hal::uart::Config::default().rx_fifo_full_threshold(READ_BUF_SIZE as u16);
+
+        Uart::new_with_config(peripherals.UART0, config, rx_pin, tx_pin)
+            .unwrap()
+            .into_async()
+    };
+
+    let serial = SerialMux::new(ble_serial, wired_serial);
+    
+
+    // CAN bus initialization
     #[cfg(feature = "esp32c3")]
     let (rx_pin, tx_pin) = (peripherals.GPIO0, peripherals.GPIO1);
 
     #[cfg(not(feature = "esp32c3"))]
     let (rx_pin, tx_pin) = (peripherals.GPIO3, peripherals.GPIO4);
 
+    let can = CanWrapper::new(peripherals.TWAI0, rx_pin, tx_pin);
     // Create the Bsp
-    let bsp = Bsp::new(CanWrapper::new(peripherals.TWAI0, rx_pin, tx_pin), ble_serial);
+    let bsp = Bsp::new(can, serial);
 
     // Create and run the Doggie core
     let core = Core::new(spawner, bsp);
@@ -124,6 +152,10 @@ async fn main(spawner: Spawner) {
     core_run!(core);
 }
 
+#[cfg(feature = "esp32c3")]
+type UartType = UsbSerialJtag<'static, Async>;
 
-core_create_tasks!(BleSerial, CanWrapper<'static>);
+#[cfg(not(feature = "esp32c3"))]
+type UartType = Uart<'static, Async>;
 
+core_create_tasks!(SerialMux<BleSerial, UartType>, CanWrapper<'static>);
---
+++ doggie_esp32/src/serial_mux.rs
@@ -0,0 +1,135 @@
+use embedded_io_async::{Read, Write, ErrorType, ErrorKind, Error};
+use embassy_futures::select::{select, Select, Either};
+use  core::convert::Infallible;
+
+#[derive(Debug)]
+pub struct SerialMuxError {}
+
+impl<'d> Error for SerialMuxError {
+    fn kind(&self) -> ErrorKind {
+        ErrorKind::Other
+    }
+}
+
+#[derive(PartialEq)]
+enum SerialMuxState {
+    Init,
+    First,
+    Second,
+}
+
+pub struct SerialMux<S1, S2>
+where
+    S1: Write + Read,
+    S2: Write + Read,
+{
+    s1: S1,
+    s2: S2,
+    state: SerialMuxState,
+}
+
+impl<S1, S2> SerialMux<S1, S2>
+where
+    S1: Write + Read,
+    S2: Write + Read,
+{
+    pub fn new(s1: S1, s2: S2) -> Self {
+        Self { s1, s2, state: SerialMuxState::Init }
+    }
+
+    async fn auto_select(&mut self) {
+        let mut buf1: [u8;1] = [0];
+        let mut buf2: [u8;1] = [0];
+
+        loop {
+            match select(
+                self.s1.read(&mut buf1),
+                self.s2.read(&mut buf2),
+            ).await {
+                Either::First(Ok(_)) => {
+                    self.state = SerialMuxState::First;
+                    return
+                },
+                Either::Second(Ok(_)) => {
+                    self.state = SerialMuxState::Second;
+                    return
+                },
+                _ => {}
+            }
+            
+        }
+    }
+}
+
+
+impl<S1, S2> ErrorType for SerialMux<S1,S2>
+where
+    S1: Write + Read,
+    S2: Write + Read,
+{
+    type Error = SerialMuxError;
+}
+
+impl<S1, S2> Read for SerialMux<S1, S2>
+where
+    S1: Write + Read,
+    S2: Write + Read,
+{
+    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        if self.state == SerialMuxState::Init {
+            self.auto_select().await;
+        }
+
+        match self.state {
+            SerialMuxState::First => {
+                match self.s1.read(buf).await {
+                    Err(_) => Err(SerialMuxError {}),
+                    Ok(res) => Ok(res),
+                }
+            },
+            SerialMuxState::Second => {
+                match self.s2.read(buf).await {
+                    Err(_) => Err(SerialMuxError {}),
+                    Ok(res) => Ok(res),
+                }
+            },
+            SerialMuxState::Init => {
+                Err(SerialMuxError {})
+            }
+        }    }
+}
+
+impl<S1, S2> Write for SerialMux<S1, S2>
+where
+    S1: Write + Read,
+    S2: Write + Read,
+{
+    async fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        if self.state == SerialMuxState::Init {
+            self.auto_select().await;
+        }
+        
+        match self.state {
+            SerialMuxState::First => {
+                match self.s1.write(buf).await {
+                    Err(_) => Err(SerialMuxError {}),
+                    Ok(res) => Ok(res),
+                }
+            },
+            SerialMuxState::Second => {
+                match self.s2.write(buf).await {
+                    Err(_) => Err(SerialMuxError {}),
+                    Ok(res) => Ok(res),
+                }
+            },
+            SerialMuxState::Init => {
+                Err(SerialMuxError {})
+            }
+        }
+    }
+
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
