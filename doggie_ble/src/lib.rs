59d3e2e5d482c810cd3436300f9e2943c7361165
Gaston Aznarez <gaznarez@faradaysec.com>
2025-06-18 10:57:54 -0300

    [BLE] Seria mux to doggiel_ble

--- doggie_ble/src/lib.rs
+++ doggie_ble/src/lib.rs
@@ -3,8 +3,10 @@
 
 pub mod constants;
 mod serial;
+mod serial_mux;
 mod server;
 pub mod types;
 
 pub use serial::BleSerial;
+pub use serial_mux::{SerialMux, SerialMuxError};
 pub use server::BleServer;
